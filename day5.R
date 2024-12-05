e8be22b547bf672b6c4eeaa762ddbd5dce2b1f7d
Sara Stoudt <sas072@bucknell.edu>
2024-12-05 08:58:04 -0500

    get day 5 ready

---
+++ day5.R
@@ -0,0 +1,11 @@
+
+#### Part 1 ####
+
+input1 <- read.csv("inputs/day5pt1.csv", header = F)
+
+## split, into different columns tidy
+
+
+input2 <- read.csv("inputs/day5pt2.csv", header = F)
+
+## get middle for everyone, then just pluck those out after verifying
