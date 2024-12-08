430115730b4a776a2db00443104a34d073b8af59
Sara Stoudt <sas072@bucknell.edu>
2024-12-08 16:18:54 -0500

    progress

--- day4.R
+++ day4.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 #### Part 1 ####
 input <- readLines("inputs/input_day4.txt")
 
@@ -11,3 +11,101 @@ input = gsub("\\\\", "", input)
 
 
 ## make matrix, check row, transpose (check cols), check diags
+
+
+ncolV = nchar(input[1])
+nrowV = length(input)
+
+
+
+
+tryThis = lapply(input, strsplit, "") %>% unlist()
+
+thisMat = matrix(tryThis, nrow = nrowV, ncol = ncolV, byrow = T)
+
+
+### rows, F & B
+numXMAS = 0
+for(i in 1:nrow(thisMat)){
+  input_sub = paste0(thisMat[i,],collapse = "")
+
+  indices = gregexpr("XMAS", input_sub)
+  numXMAS = numXMAS + regmatches(input_sub, indices) %>% unlist() %>% length()
+  
+  
+  input_subB = paste0(rev(unlist(strsplit(input_sub,""))), collapse = "")
+  
+  indices = gregexpr("XMAS", input_subB)
+  numXMAS = numXMAS + regmatches(input_subB, indices) %>% unlist() %>% length()
+  
+  
+}
+
+### columsn, F & B
+for(i in 1:ncol(thisMat)){
+  input_sub = paste0(thisMat[,i],collapse = "")
+  
+  indices = gregexpr("XMAS", input_sub)
+  numXMAS = numXMAS + regmatches(input_sub, indices) %>% unlist() %>% length()
+  
+  input_subB = paste0(rev(unlist(strsplit(input_sub,""))), collapse = "")
+  
+  indices = gregexpr("XMAS", input_subB)
+  numXMAS = numXMAS + regmatches(input_subB, indices) %>% unlist() %>% length()
+  
+}
+
+## diagonal
+
+input_sub = paste0(diag(thisMat[1:nrow(thisMat), 1:nrow(thisMat)]), collapse="")
+indices = gregexpr("XMAS", input_sub)
+numXMAS = numXMAS + regmatches(input_sub, indices) %>% unlist() %>% length()
+
+input_subB = paste0(rev(unlist(strsplit(input_sub,""))), collapse = "")
+
+indices = gregexpr("XMAS", input_subB)
+numXMAS = numXMAS + regmatches(input_subB, indices) %>% unlist() %>% length()
+
+
+## right diag F and B
+for(i in 1:(ncolV-1)){
+  test = thisMat[,-c(1:i)]
+  
+  input_sub = paste0(diag(test), collapse="")
+  
+  indices = gregexpr("XMAS", input_sub)
+  numXMAS = numXMAS + regmatches(input_sub, indices) %>% unlist() %>% length()
+  
+  input_subB = paste0(rev(unlist(strsplit(input_sub,""))), collapse = "")
+  
+  indices = gregexpr("XMAS", input_subB)
+  numXMAS = numXMAS + regmatches(input_subB, indices) %>% unlist() %>% length()
+  
+}
+
+
+## left diag F and B
+for(i in 1:(ncolV-1)){
+  test = thisMat[-c(1:i),]
+  
+  input_sub = paste0(diag(test), collapse="")
+  
+  indices = gregexpr("XMAS", input_sub)
+  numXMAS = numXMAS + regmatches(input_sub, indices) %>% unlist() %>% length()
+  
+  input_subB = paste0(rev(unlist(strsplit(input_sub,""))), collapse = "")
+  
+  indices = gregexpr("XMAS", input_subB)
+  numXMAS = numXMAS + regmatches(input_subB, indices) %>% unlist() %>% length()
+  
+}
+
+
+numXMAS
+
+
+
+
+
+thisMatR = 
+
--- day5.R
+++ day5.R
@@ -5,7 +5,54 @@ input1 <- read.csv("inputs/day5pt1.csv", header = F)
 
 ## split, into different columns tidy
 
+input1 = input1 %>% separate_wider_delim(V1, "|", names = c("first", "second") ) 
+
 
 input2 <- read.csv("inputs/day5pt2.csv", header = F)
 
 ## get middle for everyone, then just pluck those out after verifying
+
+#middleIdx = c()
+input2$middleValue <- NA
+flag <- c()
+for (i in 1:nrow(input2)) {
+  row <- unlist(strsplit(input2$V1[i], ","))
+
+
+  rules <- which(input1$first %in% row)
+#print(i)
+  for (j in 1:length(rules)) {
+    if (input1$second[rules[j]] %in% row) {
+      
+
+      
+      second <- which(row == input1$second[rules[j]])
+      first <- which(row == input1$first[rules[j]])
+#print(rules[j])
+      if (second > first) {
+        ## good
+      } else {
+        flag <- c(flag, i)
+      }
+    } else {
+
+    }
+  }
+
+  # middleIdx = c(middleIdx, ceiling(length(row)/2))
+
+  input2$middleValue[i] <- row[ceiling(length(row) / 2)]
+}
+
+
+flag %>% unique() %>% length() ## 74
+
+nrow(input2) ## 201
+
+
+input2b = input2[-unique(flag),]
+dim(input2b)
+
+sum(as.numeric(input2b$middleValue))
+
+
